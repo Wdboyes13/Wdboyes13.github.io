a0fcfc55faa9b94ec53801f4ef017efe1853ad8f
Wdboyes13 <willdev2025@outlook.com>
2025-07-15 08:15:27 -0700

    Updated Tue 15 Jul 2025 08:15:27 PDT

--- TestMobile.js
+++
@@ -1,8 +0,0 @@
-function isMobile() {
-  return /Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
-}
-if (isMobile()){
-    window.location.href = "/mobile.html"
-} else {
-    window.location.href = "/index.html"
-}
